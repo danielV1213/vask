22f2cb32335630ac443b24143f8c80d290f44de2
Daniel Vélez <daniel_andres.velez@uao.edu.co>
2023-10-17 07:20:07 -0500

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,99 @@
+<template>
+  <div class="container">
+    <Header title="Task Tracker" />
+    <Tasks :tasks="tasks" />
+  </div>
+</template>
+
+<script>
+import Header from "./components/Header.vue";
+import Tasks from "./components/Tasks.vue";
+
+export default {
+  name: "App",
+  components: {
+    Header,
+    Tasks
+  },
+  data() {
+    return {
+      tasks: [],
+    };
+  },
+
+  // The created() LifeCycle Hook is used to make httpd requests or load data when the component loads too.
+  created() {
+    this.tasks = [
+      {
+        id: 1,
+        text: "Doctors Appointment",
+        day: "March 1st at 2:30pm",
+        reminder: true,
+      },
+      {
+        id: 2,
+        text: "Job Meeting",
+        day: "March 2nd at 9:00am",
+        reminder: true,
+      },
+      {
+        id: 3,
+        text: "Gym Training",
+        day: "March 2nd at 6:30pm",
+        reminder: true,
+      },
+    ];
+  },
+};
+</script>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Poppins:wght@300;400&display=swap");
+
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  font-family: "Poppins", sans-serif;
+}
+
+.container {
+  max-width: 500px;
+  margin: 30px auto;
+  overflow: auto;
+  min-height: 300px;
+  border: 1px solid steelblue;
+  padding: 30px;
+  border-radius: 5px;
+}
+
+.btn {
+  display: inline-block;
+  background: #000;
+  color: #fff;
+  border: none;
+  padding: 10px 20px;
+  margin: 5px;
+  border-radius: 5px;
+  cursor: pointer;
+  text-decoration: none;
+  font-size: 15px;
+  font-family: inherit;
+}
+
+.btn:focus {
+  outline: none;
+}
+
+.btn:active {
+  transform: scale(0.98);
+}
+
+.btn-block {
+  display: block;
+  width: 100%;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,40 @@
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+    <Button text="Add Task" color="green" />
+    <!-- This could be reused like this -->
+    <!-- <Button text="Update Task" color="blue" /> -->
+    <!-- <Button text="Delete Task" color="red" /> -->
+  </header>
+</template>
+
+<script>
+import Button from "./Button.vue";
+
+export default {
+  name: "Header",
+  props: {
+    title: String,
+  },
+  //   There are 2 other ways to receive props from the parent component.
+  //   1. props: ['title']
+  //   2. props: {
+  //     title: {
+  //         type: String,
+  //         default: 'Hello World'
+  //     }
+  // }
+  components: {
+    Button,
+  },
+};
+</script>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+</style>
